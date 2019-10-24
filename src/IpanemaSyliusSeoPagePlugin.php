e26a92ae028931e7abd89892cb386c5cbff5c72c
VANCLOOSTER Mickael <vanclooster.mickael@gmail.com>
2019-10-24 09:22:26 +0200

    [TASK] Initial commit

---
+++ src/IpanemaSyliusSeoPagePlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ipanema\SyliusSeoPagePlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class IpanemaSyliusSeoPagePlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
